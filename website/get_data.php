037ec091405c274fd4d9fb94b281c266a56081ac
D Indra Bastari <you@example.com>
2016-08-16 03:34:17 +0700

    Website Update Versioning db

--- website/get_data.php
+++ website/get_data.php
@@ -3,25 +3,29 @@ define('HOST','localhost');
 define('USER','root');
 define('PASS','root');
 define('DB','dt');
- 
+
 $con = mysqli_connect(HOST,USER,PASS,DB);
- 
-$sql = "select * from tb_katadasar";
- 
+
+$sql = "SELECT * FROM tb_katadasar";
+
 $res = mysqli_query($con,$sql);
- 
+
 $result = array();
- 
+
+header("Content-type: application/json");
+
 while($row = mysqli_fetch_array($res)){
-array_push($result,
-array('ID'=>$row[0],
-'KATA'=>$row[1],
-'VIDEO'=>$row[2]
-));
+  array_push($result,
+    array('ID'=>$row[0],
+    'KATA'=>$row[1],
+    'LAFAL'=>$row[2],
+    'DESKRIPSI'=>$row[3],
+    'VIDEO'=>"http://10.0.3.2/dt/video/".$row[4]
+  ));
 }
- 
+
 echo json_encode(array("RESULTS"=>$result));
- 
+
 mysqli_close($con);
- 
+
 ?>
